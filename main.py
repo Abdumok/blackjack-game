93f3a76065d7e3e53ee364a7eaefbacc4286b43f
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-17 14:07:00 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+cards_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# Create Clear function
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+# Create function to show  hand
+def show_card(hand):
+     pass
+
+# Create function to determine Ace value
+def ace_value(cards):
+    pass
+
+# Create function to give card to player
+def get_card(deck):
+    pass
+
+# Create function to check for wining
+def wining(player1, player2):
+    pass
+
+
